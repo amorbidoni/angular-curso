bb5fad7f495c9547c8a3714fbc948e2e2dff3e8f
Andres Morbidoni <morbidoniandres@gmail.com>
2021-09-06 16:28:09 -0300

    commit inicial

---
+++ src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  // template: ` <h1>hola de nuevo</h1> `,
+})
+export class AppComponent {}
